b724a69d485e774901fa5a00c9c3e0ef372bc919
KelsonF <kfeduardo2003@gmail.com>
2022-08-10 21:37:46 -0300

    yes album

--- Revisão/Q2_Real_Bitcoin.js
+++ Revisão/Q2_Real_Bitcoin.js
@@ -2,8 +2,8 @@ import { input } from "../ultilidades/io.utils.js";
 
 function main(){
     let real = Number(input("Valor: "))
-    const bitcoin = 0.0000085
-    const calculadora = real / bitcoin
+    const bitcoin = 0.0000085 //o equivalente de 1 real em bitcoin
+    const calculadora = real * bitcoin
 
     console.log(calculadora.toFixed(8))
 }
