ed512e0b35d1e62d2d43e9a30742a4e89fbcc98a
KelsonF <kfeduardo2003@gmail.com>
2022-09-08 13:03:40 -0300

    ALGUMA COSA

--- Structures in C/stack.c
+++ Structures in C/stack.c
@@ -1,30 +1,64 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
 
-typedef char itemStack;
+typedef char ItemPilha;
 
-typedef struct stack {
+typedef struct pilha {
     int max;  // Capacidade da pilha
     int top; // Posição do topo
-    itemStack *item // Itens da pilha
+    ItemPilha *item // Itens da pilha
 
-} *Stack;
+} *Pilha;
 
-Stack createStack (int length){
-    Stack stack = malloc(sizeof(struct stack));  // aloca memoria
+Pilha pilha(int m){
+    Pilha P = malloc(sizeof(struct pilha)); //aloca memoria para a pilha P
+    P->max = m; // define o maximo tamanho da pilha como m
+    P->top = -1; // define a pilha, inicialmente, como vazia
+    P->item = malloc(m*sizeof(ItemPilha)); // aloca espaço para os futuros itens da pilha
 
-    stack->max = length; // capacidade pilha
-    stack->top = -1; // pilha inicialmente vazia (index)
-    stack->item = malloc(sizeof(itemStack)); 
-
-    return stack;
-
+    return P; 
 }
 
-int createEmptyStack (Stack stack){
-    return stack->top == -1; 
+int pilhaVazia(Pilha P){
+    if(P->top == -1){
+        return 1;
+    }else{
+        return 0;
+    }
 }
 
-int createFullStack (Stack stack){
-    return stack->top == stack->max -1;
+int pilhaCheia(Pilha P){
+    if(P->top == P->max - 1){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+void empilha(ItemPilha x, Pilha P){
+    if(pilhaCheia(P)){
+        puts("A pilha esta cheia !!");
+        abort();
+    }else{
+        P->top++;
+        P->item[P->top] = x;
+    }
+}
+
+Pilha desempilha(Pilha P){
+    if(pilhaVazia(P)){
+        puts("A pilha esta vazia !!");
+        abort();
+    }else{
+        ItemPilha x = P->item[P->top];
+        P->top--;
+        return x;
+    }
+}
+
+ItemPilha acessoTopo(Pilha P){
+    if(pilhaVazia(P)){
+        puts("Pilha vazia !!");
+    }else{
+        return P->item[P->top];
+    }
 }
