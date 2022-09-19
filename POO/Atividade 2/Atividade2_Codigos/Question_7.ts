18e1ea4f0b4f05c0b0aca60c8c5ace1c4fb418da
KelsonF <kfeduardo2003@gmail.com>
2022-09-19 19:25:07 -0300

    Atualizacoes necessarias

--- POO/Atividade 2/Atividade2_Codigos/Question_7.ts
+++ POO/Atividade 2/Atividade2_Codigos/Question_7.ts
@@ -1,18 +1,29 @@
+class Person{
+    name : string;
+    payment : number;
+    language : string;
+
+    constructor(name: string, payment: number, language: string){
+        this.name = name;
+        this.payment = payment;
+        this.language = language;
+    }
+
+    output(){
+        console.log(`
+            ${this.name} 
+            My payment time is ${this.payment}
+            and
+            my preffered language is ${this.language}
+        `);
+    }
+}
+
 function main() : void{
-    let name : string;
-    let payment : number;
-    let language : string;
+    let professor_ely : Person = new Person("Ely", 120.56, "TypeScript");
+    let question_answer = professor_ely.output();
 
-    name = "Ely";
-    payment = 120.56;
-    language = "TypeScript";
-
-    console.log(`
-    ${name} 
-    My payment time is ${payment}
-    and
-    my preffered language is ${language}
-    `);
+    console.log(question_answer);
 }
 
 main()
