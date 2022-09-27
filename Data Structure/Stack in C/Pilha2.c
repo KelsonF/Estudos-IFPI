26c29988120fd4a48b2146d8d8bddf6d355b2d74
KelsonF <kfeduardo2003@gmail.com>
2022-09-27 08:42:02 -0300

    Pilha usando lista encadeada

--- Data Structure/Stack in C/Pilha2.c
+++ Data Structure/Stack in C/Pilha2.c
@@ -5,20 +5,48 @@
 typedef char ItemStack;
 
 typedef struct pilha{
-    ItemStack item;
+    ItemStack value;
     struct pilha* proximo;
 }Pilha;
 
 Pilha* createItem(ItemStack valor){
-    Pilha* stack = (Pilha*)malloc(sizeof(Pilha));
-    stack->item = -1;
-    stack->proximo = NULL;
+    Pilha* item = (Pilha*)malloc(sizeof(Pilha));
+    item->value = -1;
+    item->proximo = NULL;
 
-    return stack;
+    return item;
 }
 
 Pilha* create_stack() {
 	Pilha* new_stack = create_item(0);
 
 	return new_stack;
+}
+
+bool isEmpty(Pilha* pilha){
+    return pilha->proximo = NULL; 
+}
+
+void push(int new_value, Pilha* pilha) {
+	Pilha* new_item = create_item(new_value);
+	if(pilha->proximo == NULL) {
+		pilha->proximo = new_item;
+	} else {
+		new_item->proximo
+        
+         = pilha->proximo;
+		pilha->proximo = new_item;
+	}
+}
+
+int pop(Pilha* pilha) {
+	if(!stack_is_empty(pilha)) {
+		Pilha* temp = pilha->proximo;
+		int value = temp->value;
+		pilha->proximo = pilha->proximo->proximo;
+		free(temp);
+		return value;
+	}
+
+	return '\0';
 }
