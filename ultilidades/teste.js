b01afdaf9418cb6c744b84b29b1fce8879a78bd7
KelsonF <kfeduardo2003@gmail.com>
2022-10-01 15:32:54 -0300

    .

---
+++ ultilidades/teste.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var prompt_sync_1 = require("prompt-sync");
+var input = (0, prompt_sync_1["default"])();
+var resultado = input("Alguma coisa: ");
+console.log(resultado);
---
+++ ultilidades/teste2.ts
@@ -0,0 +1,6 @@
+import PromptSync from "prompt-sync"
+const input = PromptSync();
+
+let resultado = input("Alguma coisa: ")
+console.log (resultado);
+
