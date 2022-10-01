e4edf444fc7a8b20015ba8888333bd743c2d52ea
KelsonF <kfeduardo2003@gmail.com>
2022-10-01 15:36:59 -0300

    prompt-sync funciona plmds

--- ultilidades/teste.ts
+++ ultilidades/teste.ts
@@ -1,5 +1,3 @@
-import promptSync from "prompt-sync";
-const input = promptSync();
-
-const resultado: string = input("Alguma coisa: ");
-console.log(resultado);
+var input = require('prompt-sync')();
+var teclado = input();
+console.log(teclado);
--- ultilidades/teste2.ts
+++
@@ -1,6 +0,0 @@
-import PromptSync from "prompt-sync"
-const input = PromptSync();
-
-let resultado = input("Alguma coisa: ")
-console.log (resultado);
-
