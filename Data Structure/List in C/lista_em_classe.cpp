1d59f7b433364c0409f169ea84d9da50756813dd
KelsonF <kfeduardo2003@gmail.com>
2022-10-14 12:16:53 -0300

    Atividade de Lista e seus metodos usando classe

--- Data Structure/List in C/lista_em_classe.cpp
+++ Data Structure/List in C/lista_em_classe.cpp
@@ -1,65 +1,228 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <iostream>
 using namespace std;
 
-typedef struct lista{
-	int matricula;
-	string nome;	
-}Aluno; 
-
-Aluno lista[30];
-
-//retorna a posicao do elemento procurado
-int procura(int matricula){
-    int counter = -1;
-
-    for(int i = 0; i <= counter; i++) {
-		if (lista[i].matricula == matricula) {
-			return i;
-		}
-	}
-	return -1;
-}
-
-void incluirDesordenado(Aluno novo_aluno){
-    int counter = -1;
-
-    if (procura(novo_aluno.matricula) == -1) {
-		counter++;
-		lista[counter] = novo_aluno;
-	} else {
-		cout << "Aluno ja cadastrado!" << endl;
-	}
-}
-
-//recebe a posicao e imprime o elemento na tela
-void mostrar(int posicao){
-    cout << "INFORMAÇÕES" << endl;
-    cout << "Nome: " << lista[posicao].nome << endl;
-    cout << "Matricula: " << lista[posicao].matricula << endl;
-}
-
-//procura o elemento e depois mostra o elemento encontrado (se nao for encontrado informa q ele nao existe)
-void consultar(int matricula){	
-    int posicaoElemento = procura(matricula);
-
-    if(posicaoElemento != -1){
-        mostrar(posicaoElemento);
-    } else {
-        cout << "O elemento em questão não existe" << endl;
+class No
+{
+public:
+    int mat;
+    char nome[23];
+    No *prox;
+    No(int m, char n[23])
+    {
+        mat = m;
+        strcpy(nome, n);
+        prox = NULL;
     }
-}
+};
 
-//estrat�gia 1: colocar o ultimo elemento da lista na posi��o do elemento removido
-void remover00(){
-	
-}
+class Lista
+{
+public:
+    No *inicio;
+    No *fim;
 
-//estrat�gia 2: mover TODOS os elementos que est�o AP�S  o elemento que deve ser removido UMA POSI��O A FRENTE.
-void remover01(){
-}
+    Lista()
+    {
+        inicio = NULL;
+        fim = NULL;
+    }
 
-main(){
-	
-	
+    void addToFinal(int m, char n[23])
+    {
+        No *novo = new No(m, n);
+        if (fim == NULL)
+        {
+            inicio = novo;
+            fim = novo;
+        }
+        else
+        {
+            fim->prox = novo;
+            fim = novo;
+        }
+    }
+
+    bool lista_vazia()
+    {
+        return inicio == NULL;
+    }
+
+    void addToInicio(int m, char n[23])
+    {
+        No *novoNo = new No(m, n);
+        novoNo->prox = inicio;
+        inicio = novoNo;
+
+        if (this->lista_vazia())
+        {
+            fim = novoNo;
+        }
+    }
+
+    void mostra()
+    {
+        if (lista_vazia())
+        {
+            cout << "\nLista vazia!!!";
+        }
+        else
+        {
+            No *noAux = inicio;
+
+            cout << "Lista:" << endl;
+
+            while (noAux != NULL)
+            {
+                cout << "Nome: " << noAux->nome << "|| Matricula: " << noAux->mat << endl;
+                noAux = noAux->prox;
+            }
+        }
+    }
+
+    bool remove(int mat)
+    {
+        if (this->lista_vazia())
+        {
+            cout << "Lista vazia" << endl;
+        }
+        else
+        {
+            No *noAux = inicio;
+            No *anterior = NULL;
+
+            while (noAux != NULL)
+            {
+                if (noAux->mat == mat)
+                {
+                    if (fim == noAux)
+                    {
+                        fim = anterior;
+                    }
+                    anterior->prox = noAux->prox;
+                    free(noAux);
+                    return true;
+                }
+                anterior = noAux;
+                noAux = noAux->prox;
+            }
+            cout << "Elemento nao encontrado" << endl;
+        }
+
+        return false;
+    }
+
+    // auxiliar no metodo remove, vai
+    // retornar o endereco do anterior
+    No *busca(int matricula)
+    {
+        No *noAtual = inicio;
+        No *procurado = NULL;
+        while (noAtual != NULL)
+        {
+            if (noAtual->mat == matricula)
+            {
+                procurado = noAtual;
+                break;
+            }
+            else
+            {
+                noAtual = noAtual->prox;
+            }
+        }
+
+        free(noAtual);
+        return procurado;
+    }
+
+    void removeTodos()
+    {
+        No *atual = inicio;
+        No *proximo = NULL;
+        while (atual != NULL)
+        {
+            proximo = atual->prox;
+            cout << "\n Apaga:" << atual->nome;
+            free(atual);
+            atual = proximo;
+        }
+        inicio = atual;
+    }
+
+    // criar uma nova lista que seja o inverso da primeira
+    Lista *crialistainversa()
+    {
+        if (this->lista_vazia())
+        {
+            cout << "Lista vazia" << endl;
+        }
+        else
+        {
+            Lista *lista2 = new Lista();
+
+            No *atual = inicio;
+            No *prox = inicio->prox;
+            lista2->addToInicio(inicio->mat, inicio->nome);
+
+            while (atual->prox != NULL)
+            {
+                atual = prox;
+                prox = atual->prox;
+                lista2->addToInicio(inicio->mat, inicio->nome);
+            }
+
+            return lista2;
+        }
+    }
+
+    // inverter a propria lista
+    void listainvertida()
+    {
+        if (this->lista_vazia())
+        {
+            cout << "Lista vazia" << endl;
+        }
+        else
+        {
+            No *atual = inicio;
+            No *prox = atual->prox;
+
+            No *anterior = NULL;
+            atual->prox = NULL;
+            fim = inicio;
+
+            while (prox != NULL)
+            {
+                anterior = atual;
+                atual = prox;
+                prox = atual->prox;
+
+                atual->prox = anterior;
+
+                if (prox == NULL)
+                {
+                    inicio = atual;
+                }
+            }
+        }
+    }
+};
+
+int main()
+{
+    Lista *l = new Lista();
+
+    l->addToFinal(1, "Kelson");
+    l->addToFinal(2, "Eduardo");
+    l->addToFinal(3, "Carvalho");
+    l->addToInicio(5, "Soares");
+    l->addToInicio(6, "Cristiano Oliveira");
+    l->mostra();
+
+    l->listainvertida();
+    l->mostra();
+
+    return 0;
 }
