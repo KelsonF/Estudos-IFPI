6c106015ce23a07a04913262c954745197530037
KelsonF <kfeduardo2003@gmail.com>
2022-10-20 20:29:36 -0300

    Atividade 7 POO

---
+++ POO/Átividade 7/Q2.ts
@@ -0,0 +1,17 @@
+class Calculadora2 {
+    private _operando1:number;
+    private _operando2:number;
+
+    constructor(_operando1:number,_operando2:number){
+        this._operando1 = _operando1;
+        this._operando2 = _operando2;
+    }
+
+    public soma(): number{
+        return this._operando1 + this._operando2;
+    }
+}
+
+let calculator:Calculadora2 = new Calculadora2(10,20);
+let operacao = calculator.soma();
+console.log(operacao);
