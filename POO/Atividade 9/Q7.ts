bfae42406fc7cbfa4d1fefabcc4d60c441e0a340
KelsonF <kfeduardo2003@gmail.com>
2022-11-10 23:19:24 -0300

    Atividade 9 de POO

---
+++ POO/Atividade 9/Q7.ts
@@ -0,0 +1,79 @@
+import Comparavel from './Q8'
+
+interface NewFiguraGeometrica {
+    lado: number;
+    altura: number;
+    base: number;
+
+    getArea(): number;
+
+    getPerimetro(): number;
+}
+
+class NewQuadrado implements NewFiguraGeometrica, Comparavel {
+    constructor(public lado: number, public altura: number, public base: number) { }
+
+    getArea(): number {
+        return this.lado * this.base;
+    }
+
+    getPerimetro(): number {
+        return (this.lado * 2) + (this.lado * 2)
+    }
+
+    comparar(figura: NewFiguraGeometrica): number {
+        if (this.getArea() < figura.getArea()){
+            return -1;
+        } else if(this.getArea() > figura.getArea()){
+            return 1;
+        }else {
+            return 0;
+        }
+    }
+}
+
+class NewTriangulo implements NewFiguraGeometrica,  Comparavel {
+    constructor(public lado: number, public altura: number, public base: number) { }
+
+    getArea(): number {
+        return this.base * this.altura / 2;
+    }
+
+    getPerimetro(): number {
+        return this.lado * 2 + this.base;
+    }
+
+    comparar(figura: NewFiguraGeometrica): number {
+        if (this.getArea() < figura.getArea()){
+            return -1;
+        } else if(this.getArea() > figura.getArea()){
+            return 1;
+        }else {
+            return 0;
+        }
+    }
+}
+
+class NewRetangulo implements NewFiguraGeometrica,  Comparavel {
+    constructor(public lado: number, public altura: number, public base: number) { }
+
+    getArea(): number {
+        return this.lado * this.base;
+    }
+
+    getPerimetro(): number {
+        return this.lado * 2 + this.base * 2
+    }
+
+    comparar(figura: NewFiguraGeometrica): number {
+        if (this.getArea() < figura.getArea()){
+            return -1;
+        } else if(this.getArea() > figura.getArea()){
+            return 1;
+        }else {
+            return 0;
+        }
+    }
+}
+
+export default NewFiguraGeometrica;
---
+++ POO/Atividade 9/Q8.ts
@@ -0,0 +1,7 @@
+import NewFiguraGeometrica from './Q7'
+
+interface Comparavel {
+    comparar(figura:NewFiguraGeometrica):number;
+}
+
+export default Comparavel;
